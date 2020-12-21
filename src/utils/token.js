f753f8a9e540a72ef4861cbec2b0c2563c408892
DungLe2912 <qdung.le3912@gmail.com>
2020-12-21 11:08:12 +0700

    comment function no used

--- src/utils/token.js
+++ src/utils/token.js
@@ -16,24 +16,24 @@ class Token {
     return token;
   }
 
-  static async verifyAsync(token, options = {}) {
-    const verify = promisify(jwt.verify);
-    const data = await verify(token, APP_SECRET, options);
-    return data;
-  }
+  // static async verifyAsync(token, options = {}) {
+  //   const verify = promisify(jwt.verify);
+  //   const data = await verify(token, APP_SECRET, options);
+  //   return data;
+  // }
 
-  static async sign(payload, options = {}) {
-    const token = jwt.sign(payload, APP_SECRET, {
-      ...tokenOptions,
-      ...options,
-    });
-    return token;
-  }
+  // static async sign(payload, options = {}) {
+  //   const token = jwt.sign(payload, APP_SECRET, {
+  //     ...tokenOptions,
+  //     ...options,
+  //   });
+  //   return token;
+  // }
 
-  static async verify(token, options = {}) {
-    const data = jwt.verify(token, APP_SECRET, options);
-    return data;
-  }
+  // static async verify(token, options = {}) {
+  //   const data = jwt.verify(token, APP_SECRET, options);
+  //   return data;
+  // }
 }
 
 module.exports = { Token };
